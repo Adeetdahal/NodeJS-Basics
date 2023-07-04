b288a5b4759a28c381277592f04a38cdf88f54e4
Adeetdahal <dahaladit61@gmail.com>
2023-07-04 22:32:38 +0545

    REST API using node.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const express = require('express')
-const app = express()
-
-// app.get('/', (req,res) => {
-//   res.send('Hello There!')
-// })
+const express = require('express');
+const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', require('./routes/api/users'))
 
